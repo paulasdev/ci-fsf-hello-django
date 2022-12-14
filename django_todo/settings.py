848bb4788d0558ab350cfbb723b0c2e2b8c33b79
Paula da Silva <carolpaulag@gmail.com>
2022-12-14 01:00:43 +0000

    Fixed allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-hl65k5&=u7)vzjmhd%g9a6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get['pcgs-app.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('pcgs-app.herokuapp.com')]
 
 
 # Application definition
