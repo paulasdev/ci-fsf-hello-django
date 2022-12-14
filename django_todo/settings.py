95b855441447ef58a975d96aac5da1d3436330d0
Paula da Silva <carolpaulag@gmail.com>
2022-12-14 01:20:15 +0000

    Fixed allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-hl65k5&=u7)vzjmhd%g9a6zhlh^ga@03n!!_2h8#jm72sjwco+')
+SECRET_KEY = 'django-insecure-hl65k5&=u7)vzjmhd%g9a6zhlh^ga@03n!!_2h8#jm72sjwco+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('pcgs-app.herokuapp.com')]
+ALLOWED_HOSTS = ['pcgs-app.herokuapp.com']
 
 
 # Application definition
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.parse(os.environ.get('postgres://xekooqbv:VZUMnw5vj9XVF07N_nq58fVWQLZegh19@tai.db.elephantsql.com/xekooqbv'))
 }
 
 
